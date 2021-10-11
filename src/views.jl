25f85c376a6d1d1f8309295d39cce5ae53342b12
chriselrod <elrodc@gmail.com>
2021-10-11 18:29:41 -0500

    vec fixes

--- src/views.jl
+++ src/views.jl
@@ -112,9 +112,13 @@ end
 end
 @inline Base.SubArray(A::AbstractStrideArray, i::Tuple{Vararg{<:Union{Integer,AbstractRange,Colon},K}}) where {K} = view(A, i...)
 
+@inline Base.vec(A::PtrArray{S,D,T,1,C,0}) where {S,D,T,C} = A
 @inline function Base.vec(A::PtrArray{S,D,T,N,C,0}) where {S,D,T,N,C}
   @assert all(D) "All dimensions must be dense for a vec view. Try `vec(copy(A))` instead."
-  sp = StridedPointer(pointer(A), (static_sizeof(T),), (One(),))
+  si = StrideIndex{1,(1,),1}((static_sizeof(T),), (One(),))
+  sp = stridedpointer(pointer(A), si)
   PtrArray(sp, (static_length(A),), Val((true,)))
 end
+@inline Base.vec(A::StaticStrideArray{S,D,T,1}) where {S,D,T} = A
+@inline Base.vec(A::StaticStrideArray{S,D,T,N}) where {S,D,T,N} = StrideArray(vec(PtrArray(A)), A)
 
