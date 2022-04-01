2ff101623488b4ef0cac47107c21a1f887c51ef9
Chris Elrod <elrodc@gmail.com>
2022-04-01 02:49:09 -0400

    Define vec and reshape for StrideArray

--- src/reshape.jl
+++ src/reshape.jl
@@ -7,12 +7,27 @@
 end
 @inline Base.vec(A::StaticStrideArray{S,D,T,1}) where {S,D,T} = A
 @inline Base.vec(A::StaticStrideArray{S,D,T,N}) where {S,D,T,N} = StrideArray(vec(PtrArray(A)), A)
+@inline Base.vec(A::StrideArray) = StrideArray(vec(PtrArray(A)),preserve_buffer(A))
 
-@inline function Base.reshape(A::PtrArray{S,D}, dims) where {S,D}
+@inline function Base.reshape(A::PtrArray{S,D}, dims::Tuple{Vararg{Integer}}) where {S,D}
   @assert all(D) "All dimensions must be dense for a reshaped view. Try `reshape(copy(A),...)` instead."
   PtrArray(pointer(A), dims)
 end
-@inline function Base.reshape(A::StrideArray, dims)
+@inline function Base.reshape(A::PtrArray{S,D}, dims::Tuple{Integer,Vararg{Integer}}) where {S,D}
+  @assert all(D) "All dimensions must be dense for a reshaped view. Try `reshape(copy(A),...)` instead."
+  PtrArray(pointer(A), dims)
+end
+@inline function Base.reshape(A::PtrArray{S,D}, dims::Tuple{Vararg{Int}}) where {S,D}
+  @assert all(D) "All dimensions must be dense for a reshaped view. Try `reshape(copy(A),...)` instead."
+  PtrArray(pointer(A), dims)
+end
+@inline function Base.reshape(A::StrideArray, dims::Tuple{Vararg{Integer}})
+  StrideArray(reshape(PtrArray(A), dims), preserve_buffer(A))
+end
+@inline function Base.reshape(A::StrideArray, dims::Tuple{Integer,Vararg{Integer}})
+  StrideArray(reshape(PtrArray(A), dims), preserve_buffer(A))
+end
+@inline function Base.reshape(A::StrideArray, dims::Tuple{Vararg{Int}})
   StrideArray(reshape(PtrArray(A), dims), preserve_buffer(A))
 end
 
