4251b55290efed6f7cad6ac09ab801155dd4fbf1
Chris Elrod <elrodc@gmail.com>
2022-11-21 14:31:00 -0500

    add a couple more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -300,6 +300,7 @@ allocated_cartesianindexsum(x) = @allocated cartesianindexsum(x)
                 @test view(B0, :, 4:-1:1) == view(B1, :, 4:-1:1) == B1[:, 4:-1:1]
                 @test view(B0, :, 1:2:4) == view(B1, :, 1:2:4) == B1[:,1:2:4]
                 @test view(B1, :, 4:-1:1) === B1[:, 4:-1:1]
+                @test view(B1, :, 1:2:4) === B1[:, 1:2:4]
                 A = StrideArray{Float64}(undef, (100, 100)) .= rand.()
                 vA = view(A, 3:40, 2:50)
                 @test vA === A[3:40, 2:50]
@@ -307,6 +308,7 @@ allocated_cartesianindexsum(x) = @allocated cartesianindexsum(x)
                 vaslice = view(A, 2:50, 4)
                 x = StrideArray{Float64}(undef, (100,)) .= rand.()
                 vxslice = view(x, 2:50)
+                @test vxslice === x[2:50]
                 for (i, n) in enumerate(2:50)
                         @test vaslice[i] == A[n, 4]
                         @test vxslice[i] == x[n]
